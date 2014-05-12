db3005c2f28d9515d8187652f803e399239a4441
Robert Zuber <rob@zuber.net>
2014-05-12 16:09:55 -0700

    Initial project

---
+++ SubmoduleApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SubmoduleApp
+//
+//  Created by Robert Zuber on 5/12/14.
+//  Copyright (c) 2014 Utah Street Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SAAppDelegate class]));
+    }
+}
